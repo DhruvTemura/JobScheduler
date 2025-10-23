eeed14498de250a94856adfa937294108a1328c5
DhruvTemura <dhruvtemura@gmail.com>
2025-10-23 19:43:04 +0530

    server.js validation array + empty

--- server.js
+++ server.js
@@ -7,3 +7,17 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+
+
+//input validation - returns error message if invalid
+
+function validateJobs(jobs) {
+    if (!Array.isArray(jobs)) {             //checks if jobs are array
+        return 'jobs must be an array'
+    }
+
+    if (jobs.length === 0){                 //cheks if array is empty
+        return 'jobs array cant be empty'
+    }
+}
+
